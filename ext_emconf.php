e134f6ffe42a0d3a4ccc9e911be25ad3b44202db
Sven Wappler <wappler@wappler.systems>
2024-09-25 16:01:47 +0200

    new 13 branch

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF['vhs'] = array (
     'depends' =>
     array (
       'php' => '7.4.0-8.3.99',
-      'typo3' => '10.4.0-12.4.99',
+      'typo3' => '10.4.0-13.4.99',
     ),
     'conflicts' =>
     array (
